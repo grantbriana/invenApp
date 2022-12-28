0aa232d4a30fcc30b5bc06e4cb6add9db654ee01
Briana Grant <Briana.Grant45@gmail.com>
2022-12-28 08:26:41 -0600

    Debug turned off, allowedhosts

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
