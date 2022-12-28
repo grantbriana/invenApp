0acd374e697b549f3b8f30af645bc3e3a4a42012
Briana Grant <Briana.Grant45@gmail.com>
2022-12-28 08:31:27 -0600

    syntax err

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1234567890'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
