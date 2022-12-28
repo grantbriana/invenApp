c37ff5c6dc7e5251bb32192513080f06eca2611d
Briana Grant <Briana.Grant45@gmail.com>
2022-12-28 08:35:26 -0600

    host renamed

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://invenapp.azurewebsites.net/']
 
 
 # Application definition
