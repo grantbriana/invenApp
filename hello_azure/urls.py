eafe3627ed97f92d6bedbc2398709fba009fcea3
Briana Grant <Briana.Grant45@gmail.com>
2022-12-28 13:41:06 -0600

    item CRUD

--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -7,5 +7,9 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('inventory/', views.inventory, name='inventory'),
     path("search/", views.SearchResultsView.as_view(), name="search_results"),
-
+    re_path('items/(?P<pk>\d+)$', views.ItemListView.as_view(), name='items'),
+    path('item/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
+    path('item/create/', views.ItemCreate.as_view(), name='item-create'),
+    path('item/<int:pk>/update/', views.ItemUpdate.as_view(), name='item-update'),
+    path('item/<int:pk>/delete/', views.ItemDelete.as_view(), name='item-delete'),
 ]
--- hello_azure/views.py
+++ hello_azure/views.py
@@ -8,8 +8,14 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views import generic
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q 
 
+'''
 
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
+  
 #CRUD Functionality
 class ItemCreate(CreateView):
     model = Item
@@ -23,16 +29,18 @@ class ItemDelete(DeleteView):
     model = Item
     success_url = "/hello_azure/index.html"
 
+#View item
+def item_detail_view(request, primary_key):
+  item = get_object_or_404(Item, pk=primary_key)
+  return render(request, '/item_detail.html', context={'item': item})
 
-def login(request):
-  template = loader.get_template('login.html')
-  return HttpResponse(template.render())
-
+# Home Login page
 def index(request):
     print('Request for index page received')
     return render(request, 'hello_azure/index.html')
 
 #@login_required
+#Home page
 def inventory(request):
     """View function for home page of site."""
 
@@ -53,7 +61,7 @@ def inventory(request):
 def hello(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        
+
         if name is None or name == '':
             print("Request for hello page received with no name or blank name -- redirecting")
             return redirect('index')
@@ -84,6 +92,119 @@ class ItemListView(generic.DetailView):
 class ItemDetailView(generic.DetailView):
     model = Item
 
+#@login_required
+def item_detail_view(request, primary_key):
+  item = get_object_or_404(Item, pk=primary_key)
+  return render(request, '/item_detail.html', context={'item': item})
+
+#CRUD Functionality
+class ItemCreate(CreateView):
+    model = Item
+    fields = ['id', 'name', 'inventory']
+
+class ItemUpdate(UpdateView):
+    model = Item
+    fields = ['id', 'name', 'inventory']
+
+class ItemDelete(DeleteView):
+    model = Item
+    success_url = "/inventory"
+
+#@login_required
+def create_item(request, pk):
+    item_instance = get_object_or_404(Item, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = Item(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            #item_instance.due_back = form.cleaned_data['renewal_date']
+            item_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('item'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        form = ItemCreate()
+
+    context = {
+        'form': form,
+        'item_instance': item_instance,
+    }
+
+    return render(request, 'item_form.html', context)
+
+#@login_required
+def edit_item(request, pk):
+    item_instance = get_object_or_404(Item, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = Item(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            item_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('inventory'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        form = ItemCreate(initial={'item': item})
+
+    context = {
+        'form': form,
+        'item_instance': item_instance,
+    }
+
+    return render(request, 'item_form.html', context)
+
+'''
+
+#Search Functionality
+class SearchResultsView(ListView):
+    model = Item
+    template_name = 'search_results.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get("q")
+        object_list = Item.objects.filter(
+            Q(name__icontains=query) | Q(id__icontains=query)
+        )
+        return object_list
+
+#Item Model Views
+class ItemListView(generic.DetailView):
+    model = Item
+
+class ItemDetailView(generic.DetailView):
+    model = Item
+
+
+@csrf_exempt
+def hello(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        if name is None or name == '':
+            print("Request for hello page received with no name or blank name -- redirecting")
+            return redirect('index')
+        else:
+            print("Request for hello page received with name=%s" % name)
+            context = {'name': name }
+            return render(request, 'hello_azure/hello.html', context)
+    else:
+        return redirect('index')
 
 #CRUD Functionality
 class ItemCreate(CreateView):
@@ -99,7 +220,109 @@ class ItemDelete(DeleteView):
     success_url = "/inventory"
 
 
-#@login_required
+# Home Login page
+def index(request):
+    print('Request for index page received')
+    return render(request, 'hello_azure/index.html')
+
+@login_required
 def item_detail_view(request, primary_key):
   item = get_object_or_404(Item, pk=primary_key)
   return render(request, '/item_detail.html', context={'item': item})
+
+#render pages
+@login_required
+def members(request):
+  template = loader.get_template('inventory.html')
+  return HttpResponse(template.render())
+
+
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
+
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
+
+
+@login_required
+def inventory(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_items = Item.objects.all().count()
+    item_obj = Item.objects.all()
+
+
+    context = {
+        'num_items': num_items,
+        'items': item_obj,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'inventory.html', context=context)
+
+@login_required
+def create_item(request, pk):
+    item_instance = get_object_or_404(Item, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = Item(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            #item_instance.due_back = form.cleaned_data['renewal_date']
+            item_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('item'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        form = ItemCreate()
+
+    context = {
+        'form': form,
+        'item_instance': item_instance,
+    }
+
+    return render(request, 'item_form.html', context)
+
+@login_required
+def edit_item(request, pk):
+    item_instance = get_object_or_404(Item, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = Item(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            item_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('inventory'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        form = ItemCreate(initial={'item': item})
+
+    context = {
+        'form': form,
+        'item_instance': item_instance,
+    }
+
+    return render(request, 'item_form.html', context)
+    
